549e7fb4087b860904b450c8b97cc950d1661e19
EnesT51 <99040669@mydavinci.nl>
2022-02-16 12:09:13 +0100

    mm opdracht afgerond

--- mmzak.py
+++ mmzak.py
@@ -9,10 +9,27 @@ def mmzak(aantal):
     kleuren = ["blauw", "rood","geel","bruin"]
     for i in range(aantal):
         mm [random.choice(kleuren)] +=1
-    return mm
+    return mm 
+
+kleuren = ["oranje", "blauw", "groen", "bruin"]
+
+def zak(aantal:int):
+    leeglijst =[]
+    for i in range(aantal):
+        leeglijst.append(random.choice(kleuren))
+    
+    return leeglijst
 
 keuzen = int(input("hoeveel mm wil je hebben?"))
 
-print(mmzak(keuzen))
+lijst = mmzak(keuzen)
 
+def gesorteerd(sorteer):
+    if isinstance (sorteer,list):
+        sortedd = sorted(sorteer)
+        return sortedd
+    else:
+        return dict(sorted(lijst.items(), key=lambda x:x[1]))
+
+print(gesorteerd(lijst))
 
